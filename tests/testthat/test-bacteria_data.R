ed7d10e1343ff5b6fd23ca3c1899f9e9a9fc0f92
Sourav Chatterjee <souravc83@gmail.com>
2016-02-21 15:07:19 -0800

    added test for unlabeled data

--- tests/testthat/test-bacteria_data.R
+++ tests/testthat/test-bacteria_data.R
@@ -1,15 +1,31 @@
-  library(fastBoost)
-  library(MASS)
+library(fastBoost)
+library(MASS)
   
-  test_that("Bacteria dataset works",{
+test_that("Bacteria dataset works",{
     attach(bacteria)
     boost_obj <- adaboost(y~.,bacteria , 10)
     pred <- predict(boost_obj,bacteria)
-    
-  })
+    print(paste("Adaboost Error:",pred$error))
+    detach(bacteria)
+})
 #  
- test_that("bacteria dataset works with a selection of variables",{
+test_that("bacteria dataset works with a selection of variables",{
     attach(bacteria)
     boost_obj <- adaboost(y~ap+hilo+week,bacteria , 10)
     pred <- predict(boost_obj, bacteria)
-  })
+    print(paste("Adaboost Error:",pred$error))
+    detach(bacteria)
+})
+
+test_that("bacteria dataset works with unlabeled data",{
+    attach(bacteria)
+    boost_obj <- adaboost(y~ap+hilo+week,bacteria , 10)
+    bacteria_2 <-bacteria
+    unlabeled_cols <-names(bacteria_2)[!(names(bacteria_2) %in% "y") ]
+    bacteria_2 <- bacteria_2[,unlabeled_cols]
+    pred <- predict(boost_obj, bacteria_2)
+    print(paste("Adaboost Error:",pred$error))
+    expect_true(is.na(pred$error))
+    detach(bacteria)
+})
+  
